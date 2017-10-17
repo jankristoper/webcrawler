cd6c1077b874180d9a5da2d00a968a22f25068f1
John <johnkristoper45@gmail.com>
2017-10-17 12:53:06 +0800

    some adjustment

--- semantic/crawl.js
+++ semantic/crawl.js
@@ -3,7 +3,7 @@ $('#crawl').click(function(){
     var url = $('#site').val();
     $("#siteTable  > tbody").find("tr").remove();
     $('#loader').addClass("loader ui active inline centered");
-    $("#siteTable  > tbody:last-child").append("<tr><td><div class='loader ui active inline centered'></div><h3>Please wait...</h3></td></tr>")
+    $("#siteTable  > tbody:last-child").append("<tr><td><div class='loader ui active inline centered'></div><h3 >Please wait...</h3></td></tr>")
     $.get('crawler.php?url='+url, function(result){
         $("#siteTable  > tbody").find("tr").remove();
         $.each(result, function(key, value){
